e193c1a24f1cd77831e09449efce5d6000390aee
Lyssa <ebonwolf00@gmail.com>
2022-04-07 17:10:58 +0100

    tile event listener now triggers part of the game over effect. reset button event listener partly written

--- scripts/main.js
+++ scripts/main.js
@@ -6,12 +6,11 @@ const resetGameButton = document.querySelector('.reset-button');
 
 let numberOfDragons = 10;
 let flags = 0;
-let isGameOver = false;
 
 const generateDragons = () => {
   for (let i = 0; i < 48; i++) {
     // console.log(i);
-    const tile = document.createElement('div');
+    let tile = document.createElement('div');
     tile.classList.add('game-grid-square');
     if (Math.random() < 0.2) {
       tile.classList.add('dragon');
@@ -20,14 +19,22 @@ const generateDragons = () => {
     tile.addEventListener('click', (e) => {
       e.preventDefault();
       console.log(tile.classList);
+      console.log(e.target);
       if (tile.classList.length === 1) {
         tile.style.backgroundColor = '#a5a6f6';
       } else {
-        tile.style.backgroundImage = "url('/images/Dragonsweeper-tile.png)";
-        tile.style.backgroundColor = '#a5a6f6';
+        tile.style.backgroundImage = "url('/images/Dragonsweeper-tile.png')";
+        fullGame.classList.add('game-over-box');
+        if (fullGame.classList.length === 2) {
+          console.log(fullGame.classList);
+          fullGame.style.backgroundImage = "url('/images/flames.jpg')";
+        }
       }
     });
     board.appendChild(tile);
   }
 };
+resetGameButton.addEventListener('click', (e) => {
+  const resetTiles = document.querySelectorAll('.game-grid-square');
+});
 generateDragons();
