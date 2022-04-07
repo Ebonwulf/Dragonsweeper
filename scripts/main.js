5c712b2453f58a7d01cfebaf518c424cc8a95b65
Lyssa <ebonwolf00@gmail.com>
2022-04-07 14:56:34 +0100

    rework of grid so that it is created in js rather than html, dragon generation completed and click event listener added to tiles

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,33 @@
-const board = document.querySelector('.game-grid');
+'use strict';
 
-const numberOfMines = 8;
+const fullGame = document.querySelector('.full-game-board');
+const board = document.querySelector('.game-grid');
+const resetGameButton = document.querySelector('.reset-button');
+
+let numberOfDragons = 10;
+let flags = 0;
+let isGameOver = false;
+
+const generateDragons = () => {
+  for (let i = 0; i < 48; i++) {
+    // console.log(i);
+    const tile = document.createElement('div');
+    tile.classList.add('game-grid-square');
+    if (Math.random() < 0.2) {
+      tile.classList.add('dragon');
+      console.log(i);
+    }
+    tile.addEventListener('click', (e) => {
+      e.preventDefault();
+      console.log(tile.classList);
+      if (tile.classList.length === 1) {
+        tile.style.backgroundColor = '#a5a6f6';
+      } else {
+        tile.style.backgroundImage = "url('/images/Dragonsweeper-tile.png)";
+        tile.style.backgroundColor = '#a5a6f6';
+      }
+    });
+    board.appendChild(tile);
+  }
+};
+generateDragons();
