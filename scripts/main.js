5d0d213f51bd31c49f64bb6385a329e8e08fbbec
Lyssa <ebonwolf00@gmail.com>
2022-04-11 18:44:32 +0100

    commented out the gameWin function and made some tweaks to tidy things up

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,8 @@ const board = document.querySelector('.game-grid');
 const resetGameButton = document.querySelector('.reset-button');
 const textDisplay = document.querySelector('.main-text-heading-display');
 const textDisplayBox = document.querySelector('.main-heading-box');
-
+let isGameOver = false;
+let score = 0;
 const generateDragons = () => {
   for (let i = 0; i < 64; i++) {
     // console.log(i);
@@ -24,7 +25,7 @@ const generateDragons = () => {
       } else {
         tile.style.backgroundImage = "url('/images/Dragonsweeper-tile.png')";
         fullGame.classList.add('game-over-box');
-        gameOver();
+        gameOver(tile);
       }
     });
     board.appendChild(tile);
@@ -46,33 +47,38 @@ resetGameButton.addEventListener('click', (e) => {
   generateDragons();
 });
 
-const gameOver = () => {
+const gameOver = (e) => {
+  if (e.target.className == 'game-grid-square') {
+    e.stopPropagation();
+    e.preventDefault();
+  }
   if (fullGame.classList.length === 2) {
     fullGame.style.backgroundImage = "url('/images/flames-2.jpg')";
     // const fire = `<audio src="/mixkit-fire-swoosh-burning-1328.wav"></audio>`;
     const sound = new Audio('../94102__cgeffex__fire-breathing-dragon.flac');
     sound.play();
-
     console.log(fullGame.classList);
     textDisplayBox.style.backgroundColor = 'red';
     textDisplay.innerHTML = 'GAME OVER!';
+    isGameOver = true;
   }
 };
 
-const gameWin = () => {
-  if (fullGame.classList.length === 1) {
-    textDisplay.innerHTML = 'YOU WIN!';
-    textDisplayBox.style.backgroundColor = 'rgb(12, 255, 4)';
-    fullGame.style.backgroundImage = "url('/images/smile.png')";
-    console.log(fullGame.classList);
-    if (board.style.display === 'none') {
-      board.style.display = 'block';
-    } else {
-      board.style.display = 'none';
-    }
-  }
-};
-
+// const gameWin = () => {
+//   if (!isGameOver === true) {
+//     textDisplay.innerHTML = 'YOU WIN!';
+//     textDisplayBox.style.backgroundColor = 'rgb(12, 255, 4)';
+//     fullGame.style.backgroundImage = "url('/images/smile.png')";
+//     console.log(fullGame.classList);
+//   }
+//   if (board.style.display === 'none') {
+//     board.style.display = 'block';
+//   } else {
+//     board.style.display = 'none';
+//   }
+//score ++;
+// };
+// gameWin();
 // const revealDragons = () => {
 //   const dragon = document.querySelectorAll('.dragon');
 //   if () {
