56b6c628b4665adcb6f05177a5825002d9423e1f
Lyssa <ebonwolf00@gmail.com>
2022-04-12 15:05:56 +0100

    changes made to gameOver function to register a boolean if the game is over. dragon Count and tileCount variables created and added to the generateDragons function so that we can tell when you win. winGame function still not quite right, but very close.

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,8 @@ const resetGameButton = document.querySelector('.reset-button');
 const textDisplay = document.querySelector('.main-text-heading-display');
 const textDisplayBox = document.querySelector('.main-heading-box');
 let isGameOver = false;
-let score = 0;
+let dragonCount = 0;
+let tileCount = 0;
 const generateDragons = () => {
   for (let i = 0; i < 64; i++) {
     // console.log(i);
@@ -15,30 +16,39 @@ const generateDragons = () => {
     if (Math.random() < 0.2) {
       tile.classList.add('dragon');
       console.log(i);
+      dragonCount++;
+      console.log(dragonCount);
     }
     tile.addEventListener('click', (e) => {
+      if (isGameOver) {
+        return;
+      }
       e.preventDefault();
       console.log(tile.classList);
       console.log(e.target);
       if (tile.classList.length === 1) {
         tile.style.backgroundColor = '#a5a6f6';
+        tileCount++;
+        console.log(tileCount);
       } else {
         tile.style.backgroundImage = "url('/images/Dragonsweeper-tile.png')";
         fullGame.classList.add('game-over-box');
-        gameOver(tile);
+        gameOver();
       }
     });
     board.appendChild(tile);
   }
 };
 generateDragons();
-const tiles = document.querySelectorAll('.game-grid-square');
 
 resetGameButton.addEventListener('click', (e) => {
+  isGameOver = false;
+  dragonCount = 0;
+  tileCount = 0;
   e.preventDefault();
-  // const resetTiles = document.querySelectorAll('.game-grid-square');
+  const resetTiles = document.querySelectorAll('.game-grid-square');
   board.style.display = 'grid';
-  tiles.forEach((tile) => {
+  resetTiles.forEach((tile) => {
     tile.remove('game-grid-square');
     fullGame.style.backgroundImage = 'none';
     textDisplayBox.style.backgroundColor = '#6d02c0';
@@ -48,38 +58,27 @@ resetGameButton.addEventListener('click', (e) => {
 });
 
 const gameOver = () => {
+  isGameOver = true;
   if (fullGame.classList.length === 2) {
     fullGame.style.backgroundImage = "url('/images/flames-2.jpg')";
-    // const fire = `<audio src="/mixkit-fire-swoosh-burning-1328.wav"></audio>`;
+
     const sound = new Audio('../94102__cgeffex__fire-breathing-dragon.flac');
     sound.play();
     console.log(fullGame.classList);
     textDisplayBox.style.backgroundColor = 'red';
     textDisplay.innerHTML = 'GAME OVER!';
-    isGameOver = true;
   }
 };
 
-// const gameWin = () => {
-//   if (!isGameOver === true) {
-//     textDisplay.innerHTML = 'YOU WIN!';
-//     textDisplayBox.style.backgroundColor = 'rgb(12, 255, 4)';
-//     fullGame.style.backgroundImage = "url('/images/smile.png')";
-//     console.log(fullGame.classList);
-//   }
-//   if (board.style.display === 'none') {
-//     board.style.display = 'block';
-//   } else {
-//     board.style.display = 'none';
-//   }
-//score ++;
-// };
-// gameWin();
-// const revealDragons = () => {
-//   const dragon = document.querySelectorAll('.dragon');
-//   if () {
-//     dragon.style.backgroundImage = "url('/images/Dragonsweeper-tile.png)";
-//   }
-//   console.log(dragon);
-// };
-const minesGenerated = () => {};
+const gameWin = () => {
+  isGameOver = true;
+  if (tileCount == 64 - dragonCount && !fullGame.classList.length === 2) {
+    textDisplay.innerHTML = 'YOU WIN!';
+    textDisplayBox.style.backgroundColor = 'rgb(12, 255, 4)';
+    fullGame.style.backgroundImage = "url('/images/smile.png')";
+    console.log(fullGame.classList);
+  }
+};
+
+// const minesGenerated = () => {};
+// const revealDragons = () => {};
